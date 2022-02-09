47deddae287a397d247d84e2a513f09048f1b046
Bryan VanDyke <bryan.vandyke@gmail.com>
2022-02-09 12:47:35 -0500

    Added skeleton for Unit Test for Items Class.

---
+++ app/src/test/java/com/example/studious/ItemsTest.java
@@ -0,0 +1,36 @@
+package com.example.studious;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemsTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getItemTitle() {
+    }
+
+    @Test
+    public void setItemTitle() {
+    }
+
+    @Test
+    public void getType() {
+    }
+
+    @Test
+    public void setType() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
